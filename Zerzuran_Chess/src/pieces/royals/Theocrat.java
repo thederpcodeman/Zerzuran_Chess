5db63d8b0c8173502f371a9df7a50f9c1bfe93c4
connertm24 <connermckeone42@gmail.com>
2024-06-04 08:53:42 -0700

    Theocrat can now take pieces, for a price. (6 radness, also gains 1 radness when not attacking.)

--- Zerzuran_Chess/src/pieces/royals/Theocrat.java
+++ Zerzuran_Chess/src/pieces/royals/Theocrat.java
@@ -1,6 +1,7 @@
 package Zerzuran_Chess.src.pieces.royals;
 
 import Zerzuran_Chess.src.Game.Board;
+import Zerzuran_Chess.src.Game.ChessGame;
 import Zerzuran_Chess.src.Game.Tile;
 import Zerzuran_Chess.src.pieces.Moves;
 import Zerzuran_Chess.src.pieces.Piece;
@@ -30,6 +31,30 @@ public class Theocrat extends Piece {
         int dx = newX - x;
         int dy = newY - y;
         Tile destination = board.getTile(Board.getLocationFromCords(newX, newY));
-        return ((destination.getPiece() == null || destination.getPiece().royal) && ((Moves.knightMove(dx, dy) || (Moves.bishopMove(x, y, dx, dy,board)))));
+        int r = 0;
+        if (color == 0){
+            r = Board.bRadness;
+        }else if (color == 1){
+            r = Board.wRadness;
+        }
+        if ((destination.getPiece() == null || destination.getPiece().royal || r > 5) && ((Moves.knightMove(dx, dy) || (Moves.bishopMove(x, y, dx, dy,board))))){
+            if (forReal){
+                if (destination.getPiece() == null || destination.getPiece().royal){
+                    if (color == 0){
+                        Board.bRadness++;
+                    }else if (color == 1){
+                        Board.wRadness++;
+                    }
+                }else {
+                    if (color == 0){
+                        Board.bRadness = 0;
+                    }else if (color == 1){
+                        Board.wRadness = 0;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
