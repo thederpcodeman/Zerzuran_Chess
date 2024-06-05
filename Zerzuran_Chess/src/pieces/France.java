de06d9283c0f9f1db5f77c5bfc81dc337e6b1eaf
connertm24 <connermckeone42@gmail.com>
2024-06-05 08:57:00 -0700

    france value nerf

--- Zerzuran_Chess/src/pieces/France.java
+++ Zerzuran_Chess/src/pieces/France.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 public class France extends Piece {
     public France(int color) {
         super(color);
-        value = 8;
+        value = 7;
         name = "France";
     }
 
