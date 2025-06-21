3860cf3576ef735f3f1f3b353d76c59e0c1c852b
conner <cmckeone24@georgefox.edu>
2025-06-20 17:40:20 -0700

    more AI priorities

--- Zerzuran_Chess/src/Game/moveInfo.java
+++ Zerzuran_Chess/src/Game/moveInfo.java
@@ -3,6 +3,11 @@ package Zerzuran_Chess.src.Game;
 import Zerzuran_Chess.src.Game.Board;
 import Zerzuran_Chess.src.Game.Tile;
 import Zerzuran_Chess.src.pieces.Piece;
+import Zerzuran_Chess.src.pieces.pawns.Pawn;
+import Zerzuran_Chess.src.pieces.Mage;
+
+import java.util.ArrayList;
+
 
 public class moveInfo {
     public Tile start;
@@ -18,7 +23,7 @@ public class moveInfo {
         double score = 0;
         int us = start.getPiece().getColor();
 
-        //pre move scoreing
+        //pre move scoring
         //power
         score += com.power * start.getPiece().value;
         //random
@@ -27,8 +32,8 @@ public class moveInfo {
         if (start.isPlayableMove(end.getLocationOnBoard(), board, false) == 2){
             score -= com.safety;
         }
-        //pessant
-        //add pessant code
+        //peasant
+        //add peasant code
 
         //do the move
         Piece store = end.getPiece();
@@ -42,6 +47,78 @@ public class moveInfo {
         for (Tile i : board.getOccupiedTilesOfColor(1 - us)){
             score -= com.material * i.getPiece().value;
         }
+
+        //advancement
+        for (Tile i : board.getOccupiedTiles()){
+            if (i.getPiece() instanceof Pawn || i.getPiece() instanceof Mage){
+                score += com.advancement * ((us * -2) + 1) * Board.getYFromLocation(i.getLocationOnBoard());
+            }
+        }
+        //bongcloud
+        for (Tile i : board.getOccupiedTilesOfColor(us)){
+            if (i.getPiece().royal){
+                score += com.bongcloud * ((us * -2) + 1) * Board.getYFromLocation(i.getLocationOnBoard());
+            }
+        }
+        //finisher
+        int foes = 0;
+        for (Tile i : board.getOccupiedTilesOfColor(1 - us)){
+            foes ++;
+        }
+        score -= com.finisher * Math.sqrt(foes);
+        //royalty
+        for (Tile i : board.getOccupiedTilesOfColor(us)){
+            if (i.getPiece().royal){
+                score += com.royalty;
+            }
+        }
+        for (Tile i : board.getOccupiedTilesOfColor(1 - us)){
+            if (i.getPiece().royal){
+                score -= com.royalty;
+            }
+        }
+        //control
+        boolean controled;
+        for (Tile i : board.getTiles()){
+            controled = false;
+            for (Tile j : board.getOccupiedTilesOfColor(us)){
+                if (j.isPlayableMove(i.getLocationOnBoard(), board, false) == 1){
+                    controled = true;
+                }
+            }
+            if (controled){
+                score += com.control;
+            }
+        }
+        for (Tile i : board.getTiles()){
+            controled = false;
+            for (Tile j : board.getOccupiedTilesOfColor(1 - us)){
+                if (j.isPlayableMove(i.getLocationOnBoard(), board, false) == 1){
+                    controled = true;
+                }
+            }
+            if (controled){
+                score -= com.control;
+            }
+        }
+        //center
+        ArrayList<Tile> center = new ArrayList<>();
+        center.add(board.getTile(27));
+        center.add(board.getTile(28));
+        center.add(board.getTile(35));
+        center.add(board.getTile(36));
+
+        for (Tile i : center){
+            if (i.getPiece() != null){
+                if (i.getPiece().getColor() == us){
+                    score += com.center;
+                }
+                else {
+                    score -= com.center;
+                }
+            }
+        }
+
 
 
 
